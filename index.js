14ff5926418d4142a93eccac574a704a9e694c0e
Himanshu Patkar <Himanshupatkar274@gmail.com>
2024-12-11 16:21:04 +0530

    save

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const app = require('./app');
 const server = require('http').createServer(app);
-// const serverless = require('serverless-http');
+const serverless = require('serverless-http');
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
-    server.listen(port, () => {
-        console.log(`App is running on http://localhost:${port}`);
-    });
 
-// const handler = serverless(app);
 
-module.exports = app ;
-  
+
+const handler = serverless(app);
+
+module.exports.handler = handler;
 
 
 
