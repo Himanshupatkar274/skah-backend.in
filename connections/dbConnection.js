d7d14a6663e46dfa05b2888895dbb28d29af2600
Himanshu Patkar <Himanshupatkar274@gmail.com>
2025-01-14 13:56:31 +0530

    save

--- connections/dbConnection.js
+++ connections/dbConnection.js
@@ -1,18 +1,41 @@
 const { Client } = require("pg");
 
-// Function to create and return a client instance
+// // Function to create and return a client instance
+// let client;
+// function getClient() {
+//   if (!client) {
+//     client = new Client({
+//       user: "postgres",
+//       host: "localhost",
+//       database: "himanshudb",
+//       password: "Himanshu@123",
+//       port: process.env.DBPORT || 5657,
+//     });
+//     // Connect to the database
+//     client.connect().then(() => console.log("Connected to PostgreSQL database")).catch((err) => console.error("Connection error", err));
+//   }
+//   return client;
+// }
+
+// module.exports = {
+//   getClient,
+// };
+
 let client;
 function getClient() {
   if (!client) {
     client = new Client({
-      user: "postgres",
-      host: "localhost",
-      database: "himanshudb",
-      password: "Himanshu@123",
-      port: process.env.DBPORT || 5657,
+      connectionString: process.env.DATABSE_URL, // Use Render's environment variable
+      ssl: {
+        rejectUnauthorized: false, // Required for most managed databases
+      },
     });
+
     // Connect to the database
-    client.connect().then(() => console.log("Connected to PostgreSQL database")).catch((err) => console.error("Connection error", err));
+    client
+      .connect()
+      .then(() => console.log("Connected to PostgreSQL database"))
+      .catch((err) => console.error("Connection error", err));
   }
   return client;
 }
